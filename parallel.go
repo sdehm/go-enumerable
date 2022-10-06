8ee1fa50e0b46d0a4f50ded6efba28adc5ebe043
Shaffer Dehmlow <sdehm@github.com>
2022-10-05 20:41:04 -0600

    Fix workers and track index for map

--- parallel.go
+++ parallel.go
@@ -1,12 +1,14 @@
 package enumerable
 
+import "sync"
+
 func (e Enumerable[T]) ForEachParallel(f func(T)) {
 	jobs := make(chan T, len(e.values))
 	results := make(chan bool, len(e.values))
 
 	go func() {
-		for _, value := range e.values {
-			jobs <- value
+		for _, v := range e.values {
+			jobs <- v
 		}
 		close(jobs)
 	}()
@@ -16,9 +18,52 @@ func (e Enumerable[T]) ForEachParallel(f func(T)) {
 }
 
 func worker[T any](jobs <-chan T, results chan<- bool, f func(T)) {
+	wg := sync.WaitGroup{}
 	for j := range jobs {
-		f(j)
-		results <- true
+		wg.Add(1)
+		go func(j T) {
+			defer wg.Done()
+			f(j)
+		}(j)
 	}
+	wg.Wait()
+	close(results)
+}
+
+type workItem[T any] struct {
+	value T
+	index int
+}
+
+func (e Enumerable[T]) MapParallel(f func(T) T) Enumerable[T] {
+	jobs := make(chan workItem[T], 3)
+	results := make(chan workItem[T], 3)
+
+	go func() {
+		for i, v := range e.values {
+			jobs <- workItem[T]{v, i}
+		}
+		close(jobs)
+	}()
+
+	go workerT(jobs, results, f)
+
+	for r := range results {
+		e.values[r.index] = r.value
+	}
+
+	return e
+}
+
+func workerT[T any](jobs <-chan workItem[T], results chan<- workItem[T], f func(T) T) {
+	wg := sync.WaitGroup{}
+	for j := range jobs {
+		wg.Add(1)
+		go func(j workItem[T]) {
+			results <- workItem[T]{f(j.value), j.index}
+			wg.Done()
+		}(j)
+	}
+	wg.Wait()
 	close(results)
 }
--- parallel_test.go
+++ parallel_test.go
@@ -3,6 +3,7 @@ package enumerable
 import (
 	"sync/atomic"
 	"testing"
+	"time"
 )
 
 func TestForEachParallel(t *testing.T) {
@@ -13,5 +14,26 @@ func TestForEachParallel(t *testing.T) {
 	})
 	if result.Load() != 6 {
 		t.Errorf("Expected 6, got %d", result.Load())
+	}
+}
+
+func TestMapParallel(t *testing.T) {
+	e := New([]int{1, 2, 3})
+	result := e.MapParallel(func(i int) int {
+		if i == 2 {
+			// sleep for 10 ms to make sure order is maintained
+			time.Sleep(time.Millisecond * 10)
+		}
+		return i * 2
+	}).Apply()
+	expected := New([]int{2, 4, 6})
+
+	if len(result.values) != 3 {
+		t.Errorf("Expected 3 values, got %d", len(result.values))
+	}
+	for i, v := range result.values {
+		if v != expected.values[i] {
+			t.Errorf("Expected %d, got %d", expected.values[i], v)
+		}
 	}
 }
